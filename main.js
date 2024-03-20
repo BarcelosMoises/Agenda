c8088d6ae0d363861fade57878363d6d74b46338
Moises Barcelos <barcelos_moises@yahoo.com>
2024-03-20 18:54:28 -0300

    fix: mensagem de alerta

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ function adicionaLinha(){
     const inputNumeroContato = document.getElementById('numero-contato');
 
     if(nomes.includes(inputNomeContato.value)){
-        alert(`O nome: ${inputNomeContato.value} já foi iserido`)
+        alert(`O nome: ${inputNomeContato.value} já foi inserido`)
     } else if(numeros.includes(inputNumeroContato.value)){
-        alert(`O numero: ${inputNumeroContato.value} já foi iserido`)
+        alert(`O numero: ${inputNumeroContato.value} já foi inserido`)
     } else{
         nomes.push(inputNomeContato.value);
         numeros.push(inputNumeroContato.value);
